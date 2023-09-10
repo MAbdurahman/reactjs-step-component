e75f0c7ff5ffad280a7d32588f8d39aae9716aa7
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-10 08:49:14 -0500

    Create a basic structure of the app in app/App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
 export default function App() {
-
+    const step = 1;
     return (
-        <div>
-            <h1>reactjs-step-component</h1>
-        </div>
-
+        <main className="wrapper">
+            <div className='steps'>
+                <div className="numbers">
+                    <div className={step === 1 ? "active" : ""}>1</div>
+                    <div className={step === 2 ? "active" : ""}>2</div>
+                    <div className={step === 3 ? "active" : ""}>3</div>
+                </div>
+                <p className='message'>Step {step}: {messages[step - 1]}</p>
+                <div className="buttons">
+                    <button style={{backgroundColor: "#18497A", color: "#FFF"}}>prev</button>
+                    <button style={{backgroundColor: "#18497A", color: "#FFF"}}>next</button>
+                </div>
+            </div>
+        </main>
     );
 };
