8358ab5369c92f5af8ea33f274219b5bfd50d54c
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-12 03:13:29 -0500

    Update: more responsive project

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -19,39 +19,38 @@ export default function App() {
             setStep((step) => step - 1);
         }
     }
+
     function handleNext() {
 
         if (step < 3) {
             setStep((step) => step + 1);
         }
-
     }
 
     return (
-        <>
-            <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        <main className="wrapper main">
+            <button className="close" onClick={() => setIsOpen((is) => !is)}>
                 &times;
             </button>
-            { isOpen && (
-                <main className="wrapper">
-                    <div className='steps'>
-                        <div className="numbers">
-                            <div className={step >= 1 ? "active" : ""}>1</div>
-                            <div className={step >= 2 ? "active" : ""}>2</div>
-                            <div className={step >= 3 ? "active" : ""}>3</div>
-                        </div>
-                        <p className='message'>Step {step}: {messages[step - 1]}</p>
-                        <div className="buttons">
-                            <button style={{backgroundColor: "#18497A", color: "#FFF"}}
-                                    onClick={handlePrev}>prev
-                            </button>
-                            <button style={{backgroundColor: "#18497A", color: "#FFF"}}
-                                    onClick={handleNext}>next
-                            </button>
-                        </div>
+            {isOpen && (
+                <div className='steps'>
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step >= 3 ? "active" : ""}>3</div>
                     </div>
-                </main>
+                    <p className='message'>Step {step}: {messages[step - 1]}</p>
+                    <div className="buttons">
+                        <button style={{backgroundColor: "#18497A", color: "#FFF"}}
+                                onClick={handlePrev}>prev
+                        </button>
+                        <button style={{backgroundColor: "#18497A", color: "#FFF"}}
+                                onClick={handleNext}>next
+                        </button>
+                    </div>
+                </div>
             )}
-        </>
+        </main>
+
     );
 };
